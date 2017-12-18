7ba25600520d966af274edde885f16804d4c7706
Andre Lince Faria <andregslincefaria@gmail.com>
2017-12-18 23:31:04 +0000

    refactored project

--- bat/fetch_tor_tweets.bat
+++
@@ -1,2 +0,0 @@
-call python ../main/main_tweets.py %1timeline > logs/timeline.txt
-call python ../main/main_tweets.py %1party > logs/party.txt
--- bat/rake_tweets.bat
+++
@@ -1,2 +0,0 @@
-call python ../main/main_rake.py %1timeline > logs/rake-timeline.txt
-call python ../main/main_rake.py %1party > logs/rake-party.txt
